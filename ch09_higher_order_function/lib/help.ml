2a81d8ba83f9cc0e03817850401e033e97718d5e
mingyuchoo <mingyuchoo@gmail.com>
2022-03-25 04:25:01 +0900

    updated

---
+++ ch09_higher_order_function/lib/help.ml
@@ -0,0 +1,14 @@
+open List
+
+let add1 x y = x + y
+
+let add2 = fun x -> fun y -> x + y
+
+let rec mapl1 f l =
+  match l with
+    [] -> []
+  | h::t -> map f h :: mapl1 f t
+
+let mapl2 f l = map (map f) l
+
+let mapl3 f = map (map f)
---
+++ ch09_higher_order_function/lib/help.mli
@@ -0,0 +1,7 @@
+val add1 : int -> int -> int
+val add2 : int -> int -> int
+
+
+val mapl1 : ('a -> 'b) -> 'a list list -> 'b list list
+val mapl2 : ('a -> 'b) -> 'a list list -> 'b list list
+val mapl3 : ('a -> 'b) -> 'a list list -> 'b list list
