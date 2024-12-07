373dea4fc3646bce7327f02cd527ad32689e8a80
mingyuchoo <mingyuchoo@gmail.com>
2024-12-07 11:37:32 +0900

    replaced run.sh to Makefile

--- ocaml_from_the_very_beginning/ch15_module/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-opam exec -- dune clean
-opam exec -- dune build @fmt
-opam exec -- dune build @doc
-opam exec -- dune build
-opam exec -- dune test
-opam exec -- dune exec $(basename ${PWD}) gregor.txt
