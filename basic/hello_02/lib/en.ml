46e65e0c7381e958d812c26484313d8ee311b55f
my-name <my-email@address.com>
2024-12-08 17:23:01 +0900

    updated

--- basic/hello_02/lib/en.ml
+++ basic/hello_02/lib/en.ml
@@ -1,2 +1,3 @@
 let string_of_string_list = [%show: string list]
+
 let v = String.split_on_char ' ' "hello_02 using an opam library"
--- basic/hello_02/lib/en.mli
+++ basic/hello_02/lib/en.mli
@@ -1,2 +1,3 @@
 val string_of_string_list : string list -> string
+
 val v : string list
