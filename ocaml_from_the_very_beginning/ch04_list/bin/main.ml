31a36a747537d9b88932d7ca15a0dc408645e9fd
my-name <my-email@address.com>
2024-12-09 21:28:16 +0900

    updated

--- ocaml_from_the_very_beginning/ch04_list/bin/main.ml
+++ ocaml_from_the_very_beginning/ch04_list/bin/main.ml
@@ -35,3 +35,35 @@ let () =
   |> String.concat " "
   |> ( ^ ) "My_func.odd_elements function result: "
   |> print_endline
+
+let () =
+  [1; 2; 3]
+  |> My_func.append [4; 5; 6]
+  |> List.map string_of_int
+  |> String.concat " "
+  |> ( ^ ) "My_func.append function result: "
+  |> print_endline
+
+let () =
+  [1; 2; 3; 4; 5; 6; 7; 8; 9]
+  |> My_func.reverse
+  |> List.map string_of_int
+  |> String.concat " "
+  |> ( ^ ) "My_func.reverse function result: "
+  |> print_endline
+
+let () =
+  [1; 2; 3; 4; 5; 6; 7; 8; 9]
+  |> My_func.take 4
+  |> List.map string_of_int
+  |> String.concat " "
+  |> ( ^ ) "My_func.take function result: "
+  |> print_endline
+
+let () =
+  [1; 2; 3; 4; 5; 6; 7; 8; 9]
+  |> My_func.drop 4
+  |> List.map string_of_int
+  |> String.concat " "
+  |> ( ^ ) "My_func.drop function result: "
+  |> print_endline
--- ocaml_from_the_very_beginning/ch04_list/lib/my_func.ml
+++ ocaml_from_the_very_beginning/ch04_list/lib/my_func.ml
@@ -1,142 +1,46 @@
-(*
- * let is_nil l = 
- *   match l with
- *   | [] -> true
- *   | _ -> false
- * in
- * Printf.print_endline (if is_nil [] then "true" else "false");;
- * 
- *)
 let is_nil = function
   | [] -> true
   | _ -> false
 
-(*
- * let rec length l =
- *   match l with
- *   | [] -> 0
- *   | _ :: t -> 1 + length t
- * in
- * let () = print_int (length [1;2;3;4;5;]) in
- * print_newline ()
- * ;;
- *
- *)
-
 let rec length = function
   | [] -> 0
-  | _ :: tail -> 1 + length tail
+  | _ :: t -> 1 + length t
 
-(*
- * let rec sum l =
- *   match l with
- *   | [] -> 0
- *   | h :: t -> h + sum t
- * in
- * let () = print_int (sum [1;2;3;4;5;]) in
- * print_newline ()
- * ;;
- *
- *)
 let rec sum = function
   | [] -> 0
-  | head :: tail -> head + sum tail
+  | h :: t -> h + sum t
 
-(*
- * let rec length_inner l n =
- *   match l with
- *   | [] -> n
- *   | _ :: t -> length_inner t (n + 1)
- * ;;
- *
- * let length l = length_inner l 0 in
- * let () = print_int (length [1; 2; 3; 4; 5]) in
- * print_newline ()
- *
- *)
-
-let length_inner list =
+let length_inner xs =
   (* auxiliary function; 보조함수 *)
   let rec aux acc = function
     | [] -> acc
-    | _ :: tail -> aux (acc + 1) tail
+    | _ :: t -> aux (acc + 1) t
   in
-  aux 0 list
+  aux 0 xs
 
-(*
- * let rec odd_elements l =
- *   match l with
- *   | [] -> []
- *   | [a] -> [a]
- *   | a :: _ :: t -> a :: odd_elements t
- * ;;
- *
- * let () = List.iter (printf "%d ") (odd_elements [1; 2; 3; 4; 5]) in
- * print_newline ()
- *)
-
-let rec odd_elements list =
-  match list with
+let rec odd_elements xs =
+  match xs with
   | [] -> []
-  | [head] -> [head]
-  | head :: _ :: tail -> head :: odd_elements tail
-
-(*
- * let rec append a b =
- *   match a with
- *   | [] -> b
- *   | h :: t -> h :: append t b
- * ;;
- * 
- *)
+  | [h] -> [h]
+  | h :: _ :: t -> h :: odd_elements t
 
 let rec append x1 x2 =
   match x1 with
   | [] -> x2
-  | head :: tail -> head :: append tail x2
+  | h :: t -> h :: append t x2
 
-(*
- * let () = List.iter (printf "%d ") (append [1; 2; 3; 4; 5] [6; 7; 8; 9]) in
- * print_newline ()
- * 
- * let rec rev l =
- *   match l with
- *   | [] -> []
- *   | h :: t -> rev t @ [h]
- * ;;
- * 
- *)
+let rec reverse xs =
+  match xs with
+  | [] -> []
+  | h :: t -> reverse t @ [h]
 
-(*
- * let () = List.iter (printf "%d ") (rev [1; 2; 3; 4; 5]) in
- * print_newline ()
- * 
- * let rec take n l =
- *   if n = 0 then []
- *   else
- *     match l with
- *     | [] -> []
- *     | h :: t -> h :: take (n - 1) t
- * ;;
- * 
- *)
+let rec take n = function
+  | _ when n <= 0 -> []
+  | [] -> []
+  | h :: t -> h :: take (n - 1) t
 
-(*
- * let () = List.iter (printf "%d ") (take 2 [2; 4; 6; 8; 10]) in
- * print_newline ()
- * 
- * let rec drop n l =
- *   if n = 0 then l
- *   else
- *     match l with
- *     | [] -> []
- *     | _ :: t -> drop (n - 1) t
- * ;;
- * 
- *)
-
-(*
- * let () = List.iter (printf "%d ") (drop 2 [2; 4; 6; 8; 10]) in
- * print_newline ()
- * 
- *)
+let rec drop n xs =
+  match xs with
+  | _ when n <= 0 -> xs
+  | [] -> []
+  | _ :: t -> drop (n - 1) t
