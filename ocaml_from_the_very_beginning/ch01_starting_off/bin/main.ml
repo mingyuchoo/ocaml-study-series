1787e0e929ae437a4f63e0135d8d050cef83faf0
mingyuchoo <mingyuchoo@address.com>
2025-10-24 05:14:33 +0900

    feat: initialize OCaml project for Chapter 1 exercises with build setup

---
+++ ocaml_from_the_very_beginning/ch01_starting_off/bin/main.ml
@@ -0,0 +1,64 @@
+let () =
+  Ch01_starting_off.Lib.expression_01
+  |> string_of_int
+  |> print_endline
+
+let () =
+  Ch01_starting_off.Lib.expression_02
+  |> string_of_int
+  |> print_endline
+
+let () =
+  Ch01_starting_off.Lib.expression_03
+  |> string_of_bool
+  |> print_endline
+
+let () =
+  Ch01_starting_off.Lib.expression_04
+  |> string_of_bool
+  |> print_endline
+
+let () =
+  Ch01_starting_off.Lib.expression_05
+  |> (fun c -> String.make 1 c)
+  |> print_endline
+
+let () =
+  Ch01_starting_off.Lib.expression_06
+  |> string_of_int
+  |> print_endline
+
+let () =
+  Ch01_starting_off.Lib.exercise_01
+  |> string_of_int
+  |> print_endline
+
+let () =
+  Ch01_starting_off.Lib.exercise_02
+  |> string_of_bool
+  |> print_endline
+
+let () =
+  Ch01_starting_off.Lib.exercise_03
+  |> string_of_bool
+  |> print_endline
+
+let () =
+  Ch01_starting_off.Lib.exercise_04
+  |> string_of_bool
+  |> print_endline
+
+let () =
+  Ch01_starting_off.Lib.exercise_05
+  |> string_of_bool
+  |> print_endline
+
+let () =
+  Ch01_starting_off.Lib.exercise_06
+  |> (fun c -> String.make 1 c)
+  |> print_endline
+
+let () =
+  Ch01_starting_off.Lib.exercise_07
+  |> string_of_int
+  |> print_endline
---
+++ ocaml_from_the_very_beginning/ch01_starting_off/lib/lib.ml
@@ -0,0 +1,29 @@
+let expression_01 = 1 + (2 * 3)
+
+let expression_02 = 5 * -2
+
+let expression_03 = 99 > 100
+
+let expression_04 = 4 + 3 + 2 + 1 = 10
+
+let expression_05 = 'c'
+
+let expression_06 =
+  if 100 > 99 then 0 else 1
+
+let exercise_01 = 800 / 80 / 8
+
+let exercise_02 = 1 <> 1
+
+let exercise_03 = true || false
+
+let exercise_04 = true && false
+
+let exercise_05 =
+  if true then false else true
+
+let exercise_06 = '%'
+
+(* 'a' + 'b' *)
+let exercise_07 =
+  Char.code 'a' + Char.code 'b'
