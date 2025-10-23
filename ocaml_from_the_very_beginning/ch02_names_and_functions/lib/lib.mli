00895aa70972f3ef12165afa823aa3941dec4df6
mingyuchoo <mingyuchoo@address.com>
2025-10-24 06:14:20 +0900

    feat: add OCaml function type declarations for chapter 2 exercises

---
+++ ocaml_from_the_very_beginning/ch02_names_and_functions/lib/lib.mli
@@ -0,0 +1,38 @@
+(* OCaml에서 val 키워드는 인터페이스(.mli 파일) 또는
+   시그니처(signature) 안에서 사용되는 값 선언(value
+   declaration) 키워드입니다. 즉, “이 모듈이 어떤
+   값(함수, 변수 등)을 외부에 제공할 것이다” 라고 명세하는 역할을
+   합니다. *)
+val x1 : int
+
+val x2 : int
+
+val x3 : int
+
+val x4 : int
+
+val x5 : bool
+
+val x6 : bool
+
+val x7 : bool
+
+val x8 : int
+
+val x9 : int
+
+val x10 : bool
+
+val cube : int -> int
+
+val neg : int -> bool
+
+val isvowel : char -> bool
+
+val addtoten : int -> int -> bool
+
+val factorial : int -> int
+
+val gcd : int -> int -> int
+
+val not : bool -> bool
